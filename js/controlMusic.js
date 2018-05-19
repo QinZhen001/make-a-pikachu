794f30961536a467008232cc9ff85fd7d99660d9
渣臻 <qz519189636@qq.com>
2018-05-19 13:26:46 +0800

    qz commit

---
+++ js/controlMusic.js
@@ -0,0 +1,13 @@
+var musicControl = $('#music')[0]
+musicControl.volume = 0.2
+
+$('.actions img').click(function () {
+    if(musicControl.paused){
+        musicControl.play()
+        $('.actions img').removeClass('pause')
+    }else{
+        musicControl.pause()
+        $('.actions img').addClass('pause')
+
+    }
+})
